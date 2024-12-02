99fa1cc28e7929429a74754b80305e879f788d38
Maximilian Wank <59979728+maximilianwank@users.noreply.github.com>
2024-12-02 23:17:13 +0100

    Add vaultwarden and Caddy as reverse proxy (#3)

--- README.md
+++ README.md
@@ -1,8 +1,25 @@
 # dock-n-roll
 
-Setup for my homelab with docker containers.
+Setup for my ["homelab"](https://www.raspberrypi.com/products/raspberry-pi-4-model-b/) with docker containers.
 
 ## Tips & Tricks
+
+### Backup restore
+
+If necessary, stop the containers
+```
+docker compose down
+```
+
+Unzip the archive via
+```
+unzip -P your_password_here docker_volumes_backup_{weekday}.zip -d zip_restore
+```
+
+Copy the docker_volumes folder
+```
+cp -r zip_restore/home/maxi/docker_volumes docker_volumes
+```
 
 ### :hole: Pi-hole
 
---
+++ backup_create.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# First stop all running containers that have been defined in the docker-compose.yml file
+docker-compose -f ~/dock-n-roll/docker-compose.yml down
+
+# Define the source directory and the backup file name
+BACKUP_DIR=~/docker_volumes_backups
+DAY_OF_WEEK=$(date +%A | tr '[:upper:]' '[:lower:]')
+BACKUP_FILE=$BACKUP_DIR/$DAY_OF_WEEK.zip
+SOURCE_DIR=~/docker_volumes
+PASSWORD="your_password_here"
+
+
+# Create a zip backup with a password
+zip -r -P "$PASSWORD" "$BACKUP_FILE" "$SOURCE_DIR"
+echo "Backup completed: $BACKUP_FILE"
+
+# Start all containers again
+docker-compose  -f ~/dock-n-roll/docker-compose.yml up -d

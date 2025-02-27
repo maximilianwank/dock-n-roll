4587d1c6e2d0ad5eca238a834136a2feef6c5a42
Maximilian Wank <maxi.wank@gmx.de>
2025-02-27 16:47:13 +0100

    Improve backup_create.sh with status messages for better user feedback

--- backup_create.sh
+++ backup_create.sh
@@ -8,11 +8,13 @@ DAY_OF_WEEK=$(date +%A | tr '[:upper:]' '[:lower:]')
 BACKUP_FILE=$BACKUP_DIR/$DAY_OF_WEEK.zip
 
 # Stop all running containers that have been defined in the docker-compose.yml file
+echo "Stopping containers..."
 docker-compose -f ./docker-compose.yml down
 
 # Create a zip backup with a password
+echo "Creating backup..."
 zip -r -P "$PASSWORD_BACKUP" "$BACKUP_FILE" "$DOCKER_VOLUMES"
-echo "Backup completed: $BACKUP_FILE"
 
 # Start all containers again
+echo "Starting containers..."
 docker-compose  -f ./docker-compose.yml up -d
